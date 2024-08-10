8763d9734641a5c4f85e49ad7e1e18d2130fd115
dusk-and-dawn <david.rotnicki@gmail.com>
2024-08-11 00:05:59 +0200

    added admin section and have first idea for how to present it to Picco

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about', methods= ('POST', 'GET'))
 def about():
     return render_template('about.html')
 
+@app.route('/admin', methods= ('POST', 'GET'))
+def admin():
+    return render_template('admin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
