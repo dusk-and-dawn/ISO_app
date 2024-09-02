5506e70efba70fff2cbdc984d7bbb56335b828d3
dusk-and-dawn <david.rotnicki@gmail.com>
2024-09-02 13:29:36 +0200

    cleaned up the new client section and connected it to the db

--- app.py
+++ app.py
@@ -20,8 +20,20 @@ def about():
         content = content0, content1, content2
         to_db(client, name, content)
     return render_template('about.html')
+
 @app.route('/addclient', methods=('POST', 'GET'))
 def add_client(): 
+    if request.method == 'POST':
+        client = request.form['client']
+        name = request.form['client']
+        content0 = request.form['Q1']
+        content1 = request.form['Q2']
+        content2 = request.form['Q3']
+        content3 = request.form['Q4']
+        content4 = request.form['Q5']
+        content5 = request.form['Q6']
+        content = content0, content1, content2, content3, content4, content5
+        to_db(client, name, content)
     return render_template('add_client.html')
     
 @app.route('/admin', methods= ('POST', 'GET'))
